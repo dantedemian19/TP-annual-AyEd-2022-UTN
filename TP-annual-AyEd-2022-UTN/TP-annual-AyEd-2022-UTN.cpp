a9004c88a4829713d377fa4815f425eede3b83eb
Dante Alfonso <dantedemian19@gmail.com>
2022-09-01 13:25:26 -0300

    upload comments

--- TP-annual-AyEd-2022-UTN/TP-annual-AyEd-2022-UTN.cpp
+++ TP-annual-AyEd-2022-UTN/TP-annual-AyEd-2022-UTN.cpp
@@ -1,23 +1,23 @@
 #include "../cppLibrary/menu.h" 
 #include "./classes/party.h"
 
-int main() {
+void main() {
         menuClass menu;
-        string menuTitle = "\n\t Programas: Algoritmos y Estructura de Datos 2022 K1091 Dante Alfonso \n";
+        string menuTitle = "\n\t Programa Trabajo practico \n";
         string menuText[] = {
             "start",
-            " programa 1: ejercicio de promedios",
+            " ",
             "end"
         };
         const int menuOptions = sizeof(menuText) / sizeof(menuText[0]) - 1;
-        menu.declare(menuTitle, menuText[menuOptions]);
+        menu.declare(menuTitle, menuText, menuOptions);
         while (menu.w != menu.exit) {
             menu.menu();
             wait();
             switch (menu.w)
             {
                 case 1:
-
+                    menu.showGracia();
                     break;
                 default:
                     errormens();
@@ -25,3 +25,47 @@ int main() {
             }
         }
 }
+
+/* 
+    #include <iostream>
+namespace std;
+
+int main() {
+  std::cout << "Hello World!\n";
+}
+
+(Matias)
+//TOTALES
+//EL 3%
+
+(David)
+//SACAR DE LA LISTA LOS QUE NO SUPERAN EL 3%
+//DIVISIONES
+  voit qYg(){
+	int n1,n2,quitarV,GuardarV;
+	float tresP;
+	
+	suma=n1+n2;
+	tresP=3*(suma/100);
+	if (n1<treP){
+		quitarV=n1;
+		GuardarB=n2;
+		cout<<n2;
+	}else{
+		if (n2<tresP){
+			quitarV=n2;
+			GuardarV=n1;
+			cout<<n1;
+		}
+	}
+	return 0;
+}
+
+int main(){
+	
+}
+
+(Gaston)
+//BUSCAR LOS 5 VALORES MAS GRANDES DE LA LISTA
+
+*/
